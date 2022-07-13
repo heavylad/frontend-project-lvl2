e3cb688bccdedfe3fbb80bcf72ce5d873c8ab986
heavylad <babay.pavel@gmail.com>
2022-07-13 13:14:47 +0300

    comment out format = stylish

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import parseFile from './parsers.js';
 import ast from './ast.js';
 import stylish from './formatters/stylish.js';
 
-const genDiff = (filepath1, filepath2, format = 'stylish') => {
+const genDiff = (filepath1, filepath2) => {
+  // format = 'stylish'
   const parsedFile1 = parseFile(filepath1);
   const parsedFile2 = parseFile(filepath2);
 
