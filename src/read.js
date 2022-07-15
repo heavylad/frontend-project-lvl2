ef5c41d5f05225001abe970bffcd33ebe8210677
heavylad <babay.pavel@gmail.com>
2022-07-15 15:57:38 +0300

    fix2 read.js

--- src/read.js
+++ src/read.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-underscore-dangle */
 import { fileURLToPath } from 'url';
 import { readFileSync } from 'fs';
-import { dirname, join } from 'path';
+import { dirname, resolve } from 'path';
 
 const readFile = (filename) => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
-  const getPath = () => join(__dirname, '..', '__fixtures__', filename);
+  const getPath = () => resolve(__dirname, '..', '__fixtures__', filename);
   return readFileSync(getPath(), 'utf8');
 };
 
